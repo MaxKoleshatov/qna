9829fd6ee3432b9de39fb62a9a19592e45091efa
Max <maksimkoleshatov1993@gmail.com>
2022-03-30 14:56:25 +0500

    lesson_8_fix

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,7 +1,11 @@
-class AttachmentsController < ApplicationController
+# frozen_string_literal: true
 
-    def destroy
-      @attachment = ActiveStorage::Attachment.find(params[:id])
-      @attachment.purge
-    end
+class AttachmentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def destroy
+    @attachment = ActiveStorage::Attachment.find(params[:id])
+
+    @attachment.purge if current_user.author?(@attachment.record)
+  end
 end
